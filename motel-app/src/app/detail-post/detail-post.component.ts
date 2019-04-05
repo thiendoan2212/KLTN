7d461047171fadcdde6c201986303e4bf8a17aff
Đoàn Huỳnh Thiện <thiendoanspkt@gmail.com>
2019-04-05 13:21:05 +0700

    update content detail-post

--- motel-app/src/app/detail-post/detail-post.component.ts
+++ motel-app/src/app/detail-post/detail-post.component.ts
@@ -13,6 +13,9 @@ export class DetailPostComponent implements OnInit {
     {id: 3, first: 'Larry', last: 'the Bird', handle: '@twitter'},
   ];
 
+  lat = 51.678418;
+  lng = 7.809007;
+
   headElements = ['ID', 'First', 'Last', 'Handle'];
 
   constructor() {
