d9127da2f85fd83989a47feed35aa28c9c803a00
Đoàn Huỳnh Thiện <thiendoanspkt@gmail.com>
2019-04-29 14:37:40 +0700

    comment modal search by map

--- motel-app/src/app/search/search.component.ts
+++ motel-app/src/app/search/search.component.ts
@@ -1,8 +1,10 @@
-import {Component, ElementRef, HostListener, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import {Component, HostListener, OnInit, ViewEncapsulation} from '@angular/core';
 import {SearchForm} from '../model/searchForm';
 import {Options} from 'ng5-slider';
 import {Router} from '@angular/router';
 import {GeocodingApiServiceService} from '../service/geocoding-api-service.service';
+import {MatDialog, MatDialogRef} from '@angular/material';
+import {MapsSearchComponent} from '../maps-search/maps-search.component';
 
 @Component({
   selector: 'app-search',
@@ -23,14 +25,14 @@ export class SearchComponent implements OnInit {
     hidePointerLabels: true
   };
 
-  priceOptions1: Options = {
-    floor: 0,
-    ceil: 50,
-    step: 0.5,
-    noSwitching: true,
-    hideLimitLabels: true,
-    hidePointerLabels: true
-  };
+  // priceOptions1: Options = {
+  //   floor: 0,
+  //   ceil: 50,
+  //   step: 0.5,
+  //   noSwitching: true,
+  //   hideLimitLabels: true,
+  //   hidePointerLabels: true
+  // };
 
   acreageOptions: Options = {
     floor: 0,
@@ -41,36 +43,30 @@ export class SearchComponent implements OnInit {
     hidePointerLabels: true
   };
 
-  acreageOptions1: Options = {
-    floor: 0,
-    ceil: 1000,
-    step: 5,
-    noSwitching: true,
-    hideLimitLabels: true,
-    hidePointerLabels: true
-  };
+  // acreageOptions1: Options = {
+  //   floor: 0,
+  //   ceil: 1000,
+  //   step: 5,
+  //   noSwitching: true,
+  //   hideLimitLabels: true,
+  //   hidePointerLabels: true
+  // };
   address: string;
   zoom = 13;
-  lat = 10.7756587;
-  lng = 106.7004238;
-
+  // lat = 10.7756587;
+  // lng = 106.7004238;
   public innerWidth: any;
-
-  display = 'none';
+  // display = 'none';
+  dialogMapsSearch: MatDialogRef<MapsSearchComponent>;
 
   constructor(private router: Router,
-              private geocodingAPIService: GeocodingApiServiceService) {
+              private geocodingAPIService: GeocodingApiServiceService,
+              private dialog: MatDialog) {
   }
 
   ngOnInit() {
     this.innerWidth = window.innerWidth;
     this.setValueSearchForm();
-    this.triggerFalseClick();
-  }
-
-  triggerFalseClick() {
-    const el: HTMLElement = document.getElementById('myDiv') as HTMLElement;
-    el.click();
   }
 
   @HostListener('window:resize', ['$event'])
@@ -78,19 +74,24 @@ export class SearchComponent implements OnInit {
     this.innerWidth = window.innerWidth;
   }
 
-  ClickedOut(event) {
-    if (event.target.className === 'modal') {
-      this.display = 'none';
-    }
-  }
+  // ClickedOut(event) {
+  //   if (event.target.className === 'modal') {
+  //     this.display = 'none';
+  //   }
+  // }
 
   openSearchMaps() {
-    this.display = 'block';
+    // this.display = 'block';
+    this.dialogMapsSearch = this.dialog.open(MapsSearchComponent, {
+      hasBackdrop: true,
+      width: '1140px',
+      height: '740px'
+    });
   }
 
-  closeSearchMaps() {
-    this.display = 'none';
-  }
+  // closeSearchMaps() {
+  //   this.display = 'none';
+  // }
 
   setValueSearchForm() {
     this.searchForm.priceStart = this.searchForm1.priceStart = 0;
@@ -103,38 +104,38 @@ export class SearchComponent implements OnInit {
   }
 
   navigateToSearchPage() {
-    this.router.navigate(['/ket-qua'], {queryParams: this.searchForm, skipLocationChange: false});
+    this.router.navigate(['/result'], {queryParams: this.searchForm, skipLocationChange: false});
   }
 
   navigateToSearchPage1() {
-    this.router.navigate(['/ket-qua'], {queryParams: this.searchForm1, skipLocationChange: false});
+    this.router.navigate(['/result'], {queryParams: this.searchForm1, skipLocationChange: false});
   }
-
-  mapClicked($event: any) {
-    this.searchForm1.xCoordinate = $event.coords.lat;
-    this.searchForm1.yCoordinate = $event.coords.lng;
-  }
-
-  markerDragEnd($event: any) {
-    this.searchForm1.xCoordinate = $event.coords.lat;
-    this.searchForm1.yCoordinate = $event.coords.lng;
-  }
-
-  updateLatLngFromAddress() {
-    this.geocodingAPIService
-      .findFromAddress(this.address).subscribe(response => {
-        if (response.status === 'OK') {
-          this.lat = response.results[0].geometry.location.lat;
-          this.lng = response.results[0].geometry.location.lng;
-          this.searchForm1.xCoordinate = response.results[0].geometry.location.lat;
-          this.searchForm1.yCoordinate = response.results[0].geometry.location.lng;
-          console.log('GEO ' + this.lat);
-          console.log('GEO ' + this.lng);
-        } else if (response.status === 'ZERO_RESULTS') {
-          console.log('geocodingAPIService', 'ZERO_RESULTS', response.status);
-        } else {
-          console.log('geocodingAPIService', 'Other error', response.status);
-        }
-      });
-  }
+  //
+  // mapClicked($event: any) {
+  //   this.searchForm1.xCoordinate = $event.coords.lat;
+  //   this.searchForm1.yCoordinate = $event.coords.lng;
+  // }
+  //
+  // markerDragEnd($event: any) {
+  //   this.searchForm1.xCoordinate = $event.coords.lat;
+  //   this.searchForm1.yCoordinate = $event.coords.lng;
+  // }
+  //
+  // updateLatLngFromAddress() {
+  //   this.geocodingAPIService
+  //     .findFromAddress(this.address).subscribe(response => {
+  //       if (response.status === 'OK') {
+  //         this.lat = response.results[0].geometry.location.lat;
+  //         this.lng = response.results[0].geometry.location.lng;
+  //         this.searchForm1.xCoordinate = response.results[0].geometry.location.lat;
+  //         this.searchForm1.yCoordinate = response.results[0].geometry.location.lng;
+  //         console.log('GEO ' + this.lat);
+  //         console.log('GEO ' + this.lng);
+  //       } else if (response.status === 'ZERO_RESULTS') {
+  //         console.log('geocodingAPIService', 'ZERO_RESULTS', response.status);
+  //       } else {
+  //         console.log('geocodingAPIService', 'Other error', response.status);
+  //       }
+  //     });
+  // }
 }
