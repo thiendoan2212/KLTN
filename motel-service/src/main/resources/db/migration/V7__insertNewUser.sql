7912f3b090599b91754061889c7fa80ab2f71321
Đoàn Huỳnh Thiện <thiendoanspkt@gmail.com>
2019-05-05 13:37:45 +0700

    Update V7__insertNewUser.sql

--- motel-service/src/main/resources/db/migration/V7__insertNewUser.sql
+++ motel-service/src/main/resources/db/migration/V7__insertNewUser.sql
@@ -1,4 +1,4 @@
-INSERT INTO user(email, password, full_name) VALUES ("mod@gmail.com", "$2a$10$9D5L/eHfHgJAK8G/gMCcU.sRtpWQvK9K7aq3LyOau4PgI3HkZZycK", "Mod");
+INSERT INTO user(email, password, full_name, block) VALUES ("mod@gmail.com", "$2a$10$9D5L/eHfHgJAK8G/gMCcU.sRtpWQvK9K7aq3LyOau4PgI3HkZZycK", "Mod", 0);
 
-INSERT INTO user_roles(5, 1);
-INSERT INTO user_roles(5, 3);
+INSERT INTO user_roles(user_id, role_id) VALUES(5, 1);
+INSERT INTO user_roles(user_id, role_id) VALUES(5, 3);
