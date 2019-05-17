b64cc6efba555f79cde2cbd776e2c25cf1e81c30
Đoàn Huỳnh Thiện <thiendoanspkt@gmail.com>
2019-05-17 20:00:41 +0700

    Update image.service.ts

--- motel-app/src/app/service/image.service.ts
+++ motel-app/src/app/service/image.service.ts
@@ -17,12 +17,26 @@ export class ImageService {
     return this.http.post<ImageDTO[]>(this.apiUrl + '/uploadImage/post/' + idPost, formData);
   }
 
+  addImages(idPost: number, formData: FormData): Observable<ImageDTO[]> {
+    return this.http.post<ImageDTO[]>(this.apiUrl + '/uploadMultipleFiles/post/' + idPost, formData);
+  }
+
   getImageByteByIdPost(idPost: number): Observable<Image[]> {
     return this.http.get<Image[]>(this.apiUrl + '/imageByte/post/' + idPost);
   }
 
   deleteAllImage(idPost: number) {
-    return this.http.delete(this.apiUrl + '/deleteImage/post/' + idPost);;
+    return this.http.delete(this.apiUrl + '/deleteImage/post/' + idPost);
   }
+
+  // deleteImage(listId: Array<string>): Observable<any> {
+  //   let url = this.apiUrl + '/deleteImage?id=';
+  //   for (const id of listId) {
+  //     url += (id + ',');
+  //   }
+  //   url = url.substr(0, url.length - 1);
+  //   console.log(url);
+  //   return this.http.delete<any>(url);
+  // }
 
 }
